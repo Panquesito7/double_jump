9212957f3f5eddb2e20e5c3ebea100b44346ea0b
David Leal <halfpacho@gmail.com>
2023-09-13 04:32:51 +0000

    Safety check

--- init.lua
+++ init.lua
@@ -29,10 +29,15 @@ double_jump = {
 -- Settings --
 ---------------
 
-local max_jump_number = tonumber(minetest.settings:get("double_jump.max_jump")) or 1
+local max_jump_number = tonumber(minetest.settings:get("double_jump.max_jump")) or 1 -- Builtin jump doesn't count!
 local max_jump_height = tonumber(minetest.settings:get("double_jump.max_height")) or 6.5
 local privilege_required = minetest.settings:get_bool("double_jump.privilege_required") or false
 local infinite_jumps = minetest.settings:get_bool("double_jump.infinite_jumps") or false
+
+-- Make sure the jump number is not something below zero.
+if max_jump_number <= 0 then
+    max_jump_number = 1
+end
 
 ----------------
 -- Privileges --
