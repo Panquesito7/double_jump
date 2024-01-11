ffa91a19af14f39a4c3986167b401db38b77e463
David Leal <halfpacho@gmail.com>
2024-01-11 13:16:37 -0600

    Minor updates

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 --[[
     A Minetest mod that allows the player to jump twice or more times.
-    Copyright (C) 2023-2024 David Leal (halfpacho@gmail.com)
+    Copyright (C) 2023-2024 David Leal (halfpacho@gmail.com) and contributors
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
